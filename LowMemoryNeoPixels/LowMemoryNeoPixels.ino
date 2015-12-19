e25ecf5de40b017e1dcfe88441df0a91606b1456
Jared Young <geek42-github.com@gits.org>
2015-12-19 15:59:44 -0500

    I now have it lighting up 19 pixels in order, but it does not wipe the state of the existing pixels yet, so it is just overwriting the rest. A good start, but I want it to wipe any colors that area already there, maybe just add a blanking before, with no delays.

--- LowMemoryNeoPixels/LowMemoryNeoPixels.ino
+++ LowMemoryNeoPixels/LowMemoryNeoPixels.ino
@@ -10,14 +10,20 @@
 // First up, what pin are we connected to
 #define PIN             2
 // And how many pixels we are driving. Probably > 110, as we can drive that many on the existing NeoPixel library
-#define NUMPIXELS       19
+#define NUMPIXELS      19
+//How long we delay between updates of the pixels
+#define DELAY           150
 
 // Setup some colors, as unint_32 colors so we can use them later
 // Color order is GRB
-#define     RED         0x00FF00
-#define     GREEN       0xFF0000
-#define     BLUE        0x0000FF
-#define     WHITE       0xFFFFFF
+#define     RED         0x00001100
+#define     GREEN       0x00000011
+#define     BLUE        0x00110000
+#define     YELLOW      0x00001111
+#define     MAGENTA     0x00111100
+#define     CYAN        0x00001111
+#define     WHITE       0x00111111
+#define     BLACK       0x00000000
 
 //A lot of timing understanding comes from: https://cpldcpu.wordpress.com/2014/01/14/light_ws2812-library-v2-0-part-i-understanding-the-ws2812/
 //And a lot of understanding of WS2812 bitbanging from: http://www.instructables.com/id/Bitbanging-step-by-step-Arduino-control-of-WS2811-/?ALLSTEPS
@@ -43,71 +49,95 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  colorWipe(RED, 500);
+  colorWipe(RED, DELAY);
+  colorWipe(GREEN, DELAY);
+  colorWipe(BLUE, DELAY);
+  colorWipe(WHITE, DELAY);
+  colorWipe(CYAN, DELAY);
+  colorWipe(MAGENTA, DELAY);
+  colorWipe(YELLOW, DELAY);
+  colorWipe(BLACK, DELAY);
 }
 
 //from the neopixel strandtest example, emulate the functions with some assembly
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t j=0; j<NUMPIXELS; j++) {
+  for(uint16_t j=1; j<=NUMPIXELS; j++) {
     noInterrupts(); // Need 100% focus on instruction timing
     
     volatile uint16_t
-      //i   = numBytes; // Loop counter
       i = j * 3;
     volatile uint8_t
-      //*ptr = pixels,   // Pointer to next byte
-      //b   = *ptr++,   // Current byte value
-      b = 64,
+      *ptr = (volatile uint8_t*)&c,   // Pointer to next byte
+      *pp = (volatile uint8_t*)&c + 3,
+      b   = *ptr++,   // Current byte value
+      //b = 255,
       hi,             // PORT w/output bit set high
       lo;             // PORT w/output bit set low
   
       // 20 inst. clocks per bit: HHHHHxxxxxxxxLLLLLLL
       // ST instructions:         ^   ^        ^       (T=0,5,13)
   
-      volatile uint8_t next, bit;
+      volatile uint8_t next, bit, pc;
   
       hi   = *port |  pinMask;
       lo   = *port & ~pinMask;
       next = lo;
       bit  = 8;
+      pc = 3;     //pixel color(decrements for each of a pixels 3 colors
   
       asm volatile(
-       "head20:"                   "\n\t" // Clk  Pseudocode    (T =  0)
-        "st   %a[port],  %[hi]"    "\n\t" // 2    PORT = hi     (T =  2)
-        "sbrc %[byte],  7"         "\n\t" // 1-2  if(b & 128)
-        "mov  %[next], %[hi]"      "\n\t" // 0-1   next = hi    (T =  4)
-        "dec  %[bit]"              "\n\t" // 1    bit--         (T =  5)
-        "st   %a[port],  %[next]"  "\n\t" // 2    PORT = next   (T =  7)
-        "mov  %[next] ,  %[lo]"    "\n\t" // 1    next = lo     (T =  8)
-        "breq nextbyte20"          "\n\t" // 1-2  if(bit == 0) (from dec above)
-        "rol  %[byte]"             "\n\t" // 1    b <<= 1       (T = 10)
-        "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 12)
-        "nop"                      "\n\t" // 1    nop           (T = 13)
-        "st   %a[port],  %[lo]"    "\n\t" // 2    PORT = lo     (T = 15)
-        "nop"                      "\n\t" // 1    nop           (T = 16)
-        "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 18)
-        "rjmp head20"              "\n\t" // 2    -> head20 (next bit out)
-       "nextbyte20:"               "\n\t" //                    (T = 10)
-        "ldi  %[bit]  ,  8"        "\n\t" // 1    bit = 8       (T = 11)
-        //"ld   %[byte] ,  %a[ptr]+" "\n\t" // 2    b = *ptr++    (T = 13)
-          "rjmp .+0"                 "\n\t" // 2    nop nop
-        "st   %a[port], %[lo]"     "\n\t" // 2    PORT = lo     (T = 15)
-        "nop"                      "\n\t" // 1    nop           (T = 16)
-        "sbiw %[count], 1"         "\n\t" // 2    i--           (T = 18)
-         "brne head20"             "\n"   // 2    if(i != 0) -> (next byte)
+        "head20:"                   "\n\t" // Clk  Pseudocode    (T =  0)
+          "st   %a[port],  %[hi]"    "\n\t" // 2    PORT = hi     (T =  2)
+          "sbrc %[byte],  7"         "\n\t" // 1-2  if(b & 128)
+          "mov  %[next], %[hi]"      "\n\t" // 0-1   next = hi    (T =  4)
+          "dec  %[bit]"              "\n\t" // 1    bit--         (T =  5)
+          "st   %a[port],  %[next]"  "\n\t" // 2    PORT = next   (T =  7)
+          "mov  %[next] ,  %[lo]"    "\n\t" // 1    next = lo     (T =  8)
+          "breq nextbyte20"          "\n\t" // 1-2  if(bit == 0) (from dec above)
+        
+          "rol  %[byte]"             "\n\t" // 1    b <<= 1       (T = 10)
+          "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 12)
+          "nop"                      "\n\t" // 1    nop           (T = 13)
+          "st   %a[port],  %[lo]"    "\n\t" // 2    PORT = lo     (T = 15)
+          "nop"                      "\n\t" // 1    nop           (T = 16)
+          "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 18)
+          "rjmp head20"              "\n\t" // 2    -> head20 (next bit out)
+       
+        "nextbyte20:"               "\n\t" //                    (T = 10)
+          "ldi  %[bit]  ,  8"        "\n\t" // 1    bit = 8       (T = 11)
+          "ld   %[byte] ,  %a[ptr]+" "\n\t" // 2    b = *ptr++    (T = 13)
+          //  "rjmp .+0"                 "\n\t" // 2    nop nop
+          "dec %[pc]"                "\n\t" //1
+          "nop"                      "\n\t" // 1  
+          "st   %a[port], %[lo]"     "\n\t" // 2    PORT = lo     (T = 15)
+          //"nop"                      "\n\t" // 1    nop           (T = 16)
+          "breq resetpc20"          "\n\t" // 1-2  if(pc == 0) (from dec above)
+          
+          "sbiw %[count], 1"         "\n\t" // 2    i--           (T = 18)
+          "brne head20"              "\n\t"   // 2    if(i != 0) -> (next byte)
+
+        "resetpc20:"               "\n\t"
+          "ldi  %[pc]  ,  3"        "\n\t" // 1    pc = 3       (T = 11)
+          "subi %[ptr], 4"         "\n\t" // 1  dec ptr by 4
+          "ldi  %[bit]  ,  8"        "\n\t" // 1    bit = 8       (T = 11)
+          "ld   %[byte] ,  %a[ptr]+" "\n\t" // 2    b = *ptr++    (T = 13)
+          "sbiw %[count], 1"         "\n\t" // 2    i--           (T = 18)
+          "brne head20"              "\n\t"   // 2    if(i != 0) -> (next byte)
+          
         : [port]  "+e" (port),
           [byte]  "+r" (b),
           [bit]   "+r" (bit),
           [next]  "+r" (next),
-          [count] "+w" (i)
-        //: [ptr]    "e" (ptr),
-        : [hi]     "r" (hi),
+          [count] "+w" (i),
+          [pc]    "+r" (pc)
+        : [ptr]    "e" (ptr),
+          [hi]     "r" (hi),
           [lo]     "r" (lo));
       
       interrupts();
+      delay(wait);
   }
-    delay(wait);
 }
 
