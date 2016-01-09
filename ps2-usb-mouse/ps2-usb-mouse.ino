1fdb630f581c194f0eebd67bceaecbb584acdf1e
Jared Young <geek42-github.com@gits.org>
2016-01-08 22:22:50 -0500

    Now with real mouse action. Used the trinket mouse library to emulate a mouse(and found out I had to send a null mouse movement every loop no matter what). Then mapped the arrows to the directions of mouse movement, 'a', 's', 'd' to the related mouse buttons and if you press enter you toggle auto click. It all works, both on windows, and better yet, on andriod.

--- ps2-usb-mouse/ps2-usb-mouse.ino
+++ ps2-usb-mouse/ps2-usb-mouse.ino
@@ -1,21 +1,39 @@
+//From: https://www.pjrc.com/teensy/td_libs_PS2Keyboard.html
 #include <PS2Keyboard.h>
+
+//From: https://github.com/adafruit/Adafruit-Trinket-USB
+#include <TrinketMouse.h>
 
 const int DataPin = 0;
 const int IRQpin =  2;
 
 PS2Keyboard keyboard;
 
+//used for defining super click state using enter key
+bool Click = false;
+
 void setup() {
   delay(1000);
   keyboard.begin(DataPin, IRQpin);
 
+  TrinketMouse.begin();
+
   //for blink code
   pinMode(1, OUTPUT);
-
   blink(2);
 }
 
 void loop() {
+  //click the mouse button if we have enable fast clicks
+  //otherwise just report it not moving to keep USB happy
+  //the library needs to implement: TrinketMouse.poll for non clicks really, the code is there, I might submit a patch.
+  if (Click) {
+    TrinketMouse.move(0, 0, 0, 1);
+    TrinketMouse.move(0, 0, 0, 0);
+  } else {
+    TrinketMouse.move(0, 0, 0, 0);
+  }
+  
   if (keyboard.available()) {
     
     // read the next key
@@ -23,34 +41,44 @@ void loop() {
     
     // check for some of the special keys
     if (c == PS2_ENTER) {
-      //Serial.println();
-    } else if (c == PS2_TAB) {
-      //Serial.print("[Tab]");
-    } else if (c == PS2_ESC) {
-      //Serial.print("[ESC]");
-    } else if (c == PS2_PAGEDOWN) {
-      //Serial.print("[PgDn]");
-    } else if (c == PS2_PAGEUP) {
-      //Serial.print("[PgUp]");
+      blink(1);
+      // enable auto clicking
+      Click = !Click;
     } else if (c == PS2_LEFTARROW) {
-      //Serial.print("[Left]");
-      blink(2);
+      blink(1);
+      TrinketMouse.move(-10,0,0,0);
     } else if (c == PS2_RIGHTARROW) {
-      //Serial.print("[Right]");
-      blink(3);
+      blink(1);
+      TrinketMouse.move(10,0,0,0);
     } else if (c == PS2_UPARROW) {
-      //Serial.print("[Up]");
-      blink(4);
+      blink(1);
+      TrinketMouse.move(0,-10,0,0);
     } else if (c == PS2_DOWNARROW) {
-      //Serial.print("[Down]");
-      blink(5);
-    } else if (c == PS2_DELETE) {
-      //Serial.print("[Del]");
+      blink(1);
+      TrinketMouse.move(0,10,0,0);
+
+    // and use some keys as mouse buttons  
+    } else if (c == 'a') {
+      //press 'a', click left mouse button
+      blink(1);
+      TrinketMouse.move(0,0,0,1);
+      TrinketMouse.move(0,0,0,0);
+    } else if (c == 'd') {
+      //press 'd' to click right mouse button
+      blink(1);
+      TrinketMouse.move(0,0,0,2);
+      TrinketMouse.move(0,0,0,0);
+    } else if (c == 's') {
+      //press 's' to click mouse wheel
+      blink(1);
+      TrinketMouse.move(0,0,0,4);
+      TrinketMouse.move(0,0,0,0);
     } else {
       
       // otherwise, just print all normal characters
       //Serial.print(c);
       blink(1);
+      TrinketMouse.move(random(), random(), random(), random() & 0x07);
     }
   }
 }
